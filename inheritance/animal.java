452090f8e7fbc7195106f2db1d4c78279f5012a1
UEM <aniketpaul692@gmail.com>
2025-03-25 12:17:00 +0530

    inher1

---
+++ inheritance/animal.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class animal {
+	int a;
+	void methodofchild2()
+	{
+		System.out.println("method of child");
+	}
+	static int y;
+	static void methodofchild3()
+	{
+		System.out.println("static method of child");
+	}
+
+}
---
+++ inheritance/driver1.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class driver1 {
+public static void main(String[] args) {
+	animal a=new animal();
+	dog d=new dog();
+	a.methodofchild3();
+	a.methodofchild2();
+	d.methodofchild1();
+	d.methodofchild();
+}
+}
