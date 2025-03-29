69dba14c8c2ed54a0c9665e0d6cf35ad94645118
UEM <aniketpaul692@gmail.com>
2025-03-29 13:55:10 +0530

    changes

--- caseStudy/VehicleTest.java
+++ caseStudy/VehicleTest.java
@@ -1,43 +1,50 @@
 package caseStudy;
 
-
 import java.util.Scanner;
 
 public class VehicleTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-     
+        // Creating vehicle objects
         Vehicle car = new Car("BMW", 60);
         Vehicle bike = new Bike("Royal Enfield", 20);
         Vehicle truck = new Truck("Volvo", 40);
 
-      
-        car.start();
-        car.displaySpeed();
-        car.accelerate(20);
-        car.brake(10);
-        System.out.println("Current Speed: " + car.getCurrentSpeed() + " km/h");
-        car.stop();
-        System.out.println("------------------------");
+        while (true) {
+            System.out.println("Select a vehicle to display details:");
+            System.out.println("1. Car\n2. Bike\n3. Truck\n4. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
 
-        bike.start();
-        bike.displaySpeed();
-        bike.accelerate(15);
-        bike.brake(5);
-        System.out.println("Current Speed: " + bike.getCurrentSpeed() + " km/h");
-        bike.stop();
-        System.out.println("------------------------");
+            switch (choice) {
+                case 1:
+                    printVehicleDetails(car);
+                    break;
+                case 2:
+                    printVehicleDetails(bike);
+                    break;
+                case 3:
+                    printVehicleDetails(truck);
+                    break;
+                case 4:
+                    System.out.println("Exiting program...");
+                    sc.close();
+                    return; // Exit the program
+                default:
+                    System.out.println("Invalid choice. Please select again.");
+            }
+            System.out.println("------------------------");
+        }
+    }
 
-        truck.start();
-        truck.displaySpeed();
-        truck.accelerate(30);
-        truck.brake(20);
-        System.out.println("Current Speed: " + truck.getCurrentSpeed() + " km/h");
-        truck.stop();
-        System.out.println("------------------------");
-
-       
-        sc.close();
+    // Method to print vehicle details
+    public static void printVehicleDetails(Vehicle vehicle) {
+        vehicle.start();
+        vehicle.displaySpeed();
+        vehicle.accelerate(20);
+        vehicle.brake(10);
+        System.out.println("Current Speed: " + vehicle.getCurrentSpeed() + " km/h");
+        vehicle.stop();
     }
 }
