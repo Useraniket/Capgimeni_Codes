a8be9ae4e4f5f544e70f8dff94fc54234381edc2
UEM <aniketpaul692@gmail.com>
2025-04-19 16:01:24 +0530

    new

--- practice/ex1.java
+++ practice/ex1.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class ex1 {
+import java.util.*;
 
+public class ex1 {
+	public static void main(String[] args) {
+		ArrayList<Double> arr = new ArrayList<Double>();
+		arr.add(18.0);
+		arr.add(19.2);
+		arr.add(12.0);
+		arr.add(10.8);
+
+
+       	arr.sort((n1, n2) -> Double.compare(n2,n1));
+
+		System.out.println("Sorted in reverse:" + arr);
+		
+	}
 }
